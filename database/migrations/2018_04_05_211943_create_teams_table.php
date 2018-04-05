c05b7a1fc6d96fb0670b7eb1a63133226e2a2f26
Tom Lynch <tom@lnch.co.uk>
2018-04-05 23:43:13 +0100

    Add Team model, migration, config file and DB seeder

---
+++ database/migrations/2018_04_05_211943_create_teams_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char("code", 3)->unique();
+            $table->char("display_code", 3);
+            $table->string("name");
+            $table->integer("fifa_ranking");
+            $table->char("group", 1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teams');
+    }
+}
---
+++ database/seeds/TeamSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Team;
+use Illuminate\Database\Seeder;
+
+class TeamSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $teams = config("teams");
+        foreach ($teams as $code => $info)
+        {
+            // Create data array
+            $dataInfo = explode(",", $info);
+            $data = [
+                'code' => $code,
+                'name' => $dataInfo[0],
+                'display_code' => $dataInfo[1] !== "" ? $dataInfo[1] : $code,
+                'fifa_ranking' => $dataInfo[2],
+                'group' => $dataInfo[3]
+            ];
+            
+            // Check if team exists
+            $team = Team::where("code", $code)->first();
+            if ($team)
+            {
+                $team->update($data);                
+            }
+            else
+            {
+                $team = Team::create($data);
+            }
+        }
+    }
+}
